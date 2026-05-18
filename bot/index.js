6139a44a0109926fd4fb9b62f95b5beeb77a2aca
hihivuiqua6-create <hihivuiqua6@gmail.com>
2026-05-18 12:40:20 +0700

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
 // dán TOKEN MỚI vào đây
-const token = "7854245357:AAGUnxhw1737o-x4bqPL9DkxZjBDqKkxnic
-";
+const token = "7854245357:AAGUnxhw1737o-x4bqPL9DkxZjBDqKkxnic";
 
 const bot = new TelegramBot(token,{
     polling:true
